204c5092878ff263af388ab48c29e70d7f0e747f
User <kishanthorat5@gmail.com>
2023-03-26 16:00:56 +0530

    Edit test1 and test2 to Branch-X

--- src/com/github/Test1.java
+++ src/com/github/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test1 is done");
+		System.out.println("Test-1 is done");
 	}
 
 }
--- src/com/github/Test2.java
+++ src/com/github/Test2.java
@@ -4,6 +4,6 @@ public class Test2 {
 
 public static void main(String[] args) {
 		
-		System.out.println("Test2 is done");
+		System.out.println("Test-2 is done");
 	}
 }
